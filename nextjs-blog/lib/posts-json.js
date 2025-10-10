27d67eb416fc81f2a594b975c995247ceea71175
Melanie <1958Melanie.Castillo@gmail.com>
2025-10-09 21:42:45 -0700

    commit

--- nextjs-blog/lib/posts-json.js
+++ nextjs-blog/lib/posts-json.js
@@ -39,23 +39,6 @@ export function getPostData(id) {
     const filePath = path.join(dataDir, 'posts.json');
     const jsonString = fs.readFileSync(filePath, 'utf8');
     const jsonObj = JSON.parse(jsonString);
-    jsonObj.sort(function(a, b) {
-    return a.title.localeCompare(b.title);
-  });
-  return jsonObj.map(item => {
-    return {
-        id: item.id.toString(),
-        title: item.title,
-        date: item.date,
-    }
-  });
-
-}
-
-export function getPostData(id) {
-    const filePath = path.join(dataDir, 'posts.json');
-    const jsonString = fs.readFileSync(filePath, 'utf8');
-    const jsonObj = JSON.parse(jsonString);
     const objReturned = jsonObj.filter(obj => {
         return obj.id.toString() === id;
     });
